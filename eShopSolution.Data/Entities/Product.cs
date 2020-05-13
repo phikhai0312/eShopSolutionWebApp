e60475cc1c087f6dec0827a944b4d0cb3aa3dfcf
Công Nguyễn Tiến <17520299@ms.uit.edu.vn>
2020-05-13 15:00:14 +0700

    create successfully

--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -13,5 +13,10 @@ namespace eShopSolution.Data.Entities
         public int ViewCount { set; get; }
         public DateTime DateCreated { set; get; }
        public string SeoAlias { set; get; }
+        public List<ProductInCategory> ProductInCategories { set; get; }
+        public List<OrderDetail> OrderDetails { set; get; }
+        public List<Cart> Carts { get; set; }
+
+        public List<ProductTranslation> ProductTranslations { get; set; }
     }
 }
